9074af4f016658df561421460fcbc74375dd6551
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-23 11:12:20 -0300

    feat: add basic logger in controller

--- logs/src/main/java/br/com/alura/logs/controller/CursoController.java
+++ logs/src/main/java/br/com/alura/logs/controller/CursoController.java
@@ -1,5 +1,6 @@
 package br.com.alura.logs.controller;
 
+import br.com.alura.logs.exceptions.InternalErrorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -35,58 +37,75 @@ public class CursoController {
 	
 	@PostMapping
 	 public ResponseEntity<Object> saveCurso(@RequestBody @Valid CursoDto cursoDto){
-		
+		logger.info("iniciando processo de inserção de registro de novo curso");
+    logger.info("Chamando o curso service para validar se o numero de matricula já existe!");
+
 		if(cursoService.existsByNumeroMatricula(cursoDto.getNumeroMatricula())) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("O número de matricula do curso já esta em uso!");
+			logger.warn("novo registro não inserido, o numero da matricula já existe!");
+      return ResponseEntity.status(HttpStatus.CONFLICT).body("O número de matricula do curso já esta em uso!");
 		}
-		
+
+    logger.info("Chamando o curso service para validar se o numero do curso já existe!");
 		if(cursoService.existsByNumeroCurso(cursoDto.getNumeroCurso())) {
+      logger.warn("novo registro não inserido, o numero do curso já existe");
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("O número do curso já esta em uso!");
 		}
-				
+
+    logger.info("Validações de cursoService sobre cursoDto executadas com sucesso!");
+    logger.info("Chamando cursoService.save para armazenar novo registro...");
 		var cursoModel = new CursoModel();
 		BeanUtils.copyProperties(cursoDto, cursoModel);
 		cursoModel.setDataInscricao(LocalDateTime.now(ZoneId.of("UTC")));
+    logger.info("Novo registro de curso salvo com sucesso");
 		return ResponseEntity.status(HttpStatus.CREATED).body(cursoService.save(cursoModel));
 	}
 	
 	
 	@GetMapping
 	public ResponseEntity<Page<CursoModel>> getAllCursos(@PageableDefault(page = 0, size = 10, sort = "dataInscricao", direction = Sort.Direction.ASC) Pageable pageable) {
-		logger.info("Buscando por todos os registros");
-    return ResponseEntity.status(HttpStatus.OK).body(cursoService.findAll(pageable));
-	}
+		try {
+      logger.info("Chamando cursoService para buscar todos os registros");
+      return ResponseEntity.status(HttpStatus.OK).body(cursoService.findAll(pageable));
+    } catch (CannotCreateTransactionException ex) {
+      logger.error("Erro de comunicação com o database");
+      throw new InternalErrorException("Erro momentâneo, por favor tente mais tarde...");
+    }
+  }
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Object> getOneCursos(@PathVariable(value="id") UUID id) {
-		Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
-        if (!cursoModelOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(cursoModelOptional.get());
+    logger.info("Chamando cursoService para buscar um registro por UUID");
+    Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
+    logger.info("Validando por cursoService se o UUID existe");
+    if (!cursoModelOptional.isPresent()) {
+      logger.warn("Validação em cursoService não encontrou o registro procurado!");
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
     }
+    logger.info("O registro procurado pelo cliente foi encontrado por cursoService no database");
+    return ResponseEntity.status(HttpStatus.OK).body(cursoModelOptional.get());
+  }
 	
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteCursos(@PathVariable(value = "id") UUID id){
-        Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
-        if (!cursoModelOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
-        }
-        cursoService.delete(cursoModelOptional.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Curso excluído com sucesso!");
-    }
-    
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateCursos(@PathVariable(value = "id") UUID id, @RequestBody @Valid CursoDto cursoDto) {
-        Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
-        if (!cursoModelOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
-        }
-        var cursoModel = new CursoModel();
-        BeanUtils.copyProperties(cursoDto, cursoModel);
-        cursoModel.setId(cursoModelOptional.get().getId());
-        cursoModel.setDataInscricao(cursoModelOptional.get().getDataInscricao());
-        return ResponseEntity.status(HttpStatus.OK).body(cursoService.save(cursoModel));
-    }
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> deleteCursos(@PathVariable(value = "id") UUID id){
+      Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
+      if (!cursoModelOptional.isPresent()) {
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
+      }
+      cursoService.delete(cursoModelOptional.get());
+      return ResponseEntity.status(HttpStatus.OK).body("Curso excluído com sucesso!");
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateCursos(@PathVariable(value = "id") UUID id, @RequestBody @Valid CursoDto cursoDto) {
+      Optional<CursoModel> cursoModelOptional = cursoService.findById(id);
+      if (!cursoModelOptional.isPresent()) {
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado!");
+      }
+      var cursoModel = new CursoModel();
+      BeanUtils.copyProperties(cursoDto, cursoModel);
+      cursoModel.setId(cursoModelOptional.get().getId());
+      cursoModel.setDataInscricao(cursoModelOptional.get().getDataInscricao());
+      return ResponseEntity.status(HttpStatus.OK).body(cursoService.save(cursoModel));
+  }
 
 }
